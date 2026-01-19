b0fd372715787232c6d568157f8b1df911711b9a
时习 <xiaojunjin.xjj@alibaba-inc.com>
2026-01-19 18:07:53 +0800

    .

--- run_nebula_libero_plus.sh
+++ run_nebula_libero_plus.sh
@@ -54,4 +54,4 @@ nebulactl run mdl --queue=amap-poi_ppu810e \
                   --job_name="libero_plus" \
                   --nas_file_system_id=92bcb4b594-nvt70.cn-zhangjiakou.nas.aliyuncs.com,29016449f1c-mkq60.cn-wulanchabu.nas.aliyuncs.com,9dc4e499f2-tek11.cn-zhangjiakou.nas.aliyuncs.com,29e2cf482cb-cxw73.cn-wulanchabu.nas.aliyuncs.com \
                   --nas_file_system_mount_path=/mnt/workspace,/mnt/nas-data-3,/mnt/nas-data-1,/mnt/xlab-nas-1 \
-                  --custom_docker_image=hub.docker.alibaba-inc.com/mdl/notebook_saved:xiaojunjin.xjj_libero_plus_ppu810e_20260119165237
+                  --custom_docker_image=hub.docker.alibaba-inc.com/mdl/notebook_saved:xiaojunjin.xjj_libero_plus_ppu2_20260119180727
