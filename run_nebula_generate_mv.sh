f37f4d41b3e94119bc0eaa93d5ad1f1ffc6f284b
时习 <xiaojunjin.xjj@alibaba-inc.com>
2026-01-23 10:19:49 +0800

    .

--- run_nebula_generate_mv.sh
+++ run_nebula_generate_mv.sh
@@ -37,7 +37,7 @@ for i in "${!sizes[@]}"; do
                   --job_name="libero" \
                   --nas_file_system_id=92bcb4b594-nvt70.cn-zhangjiakou.nas.aliyuncs.com,29016449f1c-mkq60.cn-wulanchabu.nas.aliyuncs.com,9dc4e499f2-tek11.cn-zhangjiakou.nas.aliyuncs.com,29e2cf482cb-cxw73.cn-wulanchabu.nas.aliyuncs.com \
                   --nas_file_system_mount_path=/mnt/workspace,/mnt/nas-data-3,/mnt/nas-data-1,/mnt/xlab-nas-1 \
-                  --custom_docker_image=hub.docker.alibaba-inc.com/mdl/notebook_saved:xiaojunjin.xjj_libero_plus_4_20260106181910
+                  --custom_docker_image=hub.docker.alibaba-inc.com/mdl/notebook_saved:xiaojunjin.xjj_libero_plus_ppu2_20260119180727
         start_idx=$end_idx
     done
 done
