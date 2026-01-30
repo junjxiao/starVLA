235201022680d13292e9f7b7b592f56fa8627a10
时习 <xiaojunjin.xjj@alibaba-inc.com>
2026-01-30 19:23:52 +0800

    .

--- examples/Robotwin/eval_files/eval_nebula/eval.sh
+++ examples/Robotwin/eval_files/eval_nebula/eval.sh
@@ -10,11 +10,11 @@ port=5695
 gpu_id=0
 policy_ckpt_path=/mnt/workspace/junjin/code/starVLA/checkpoints/0127_robotwin_Qwen3vlGR00T_vggt_cross_bs16/checkpoints/steps_10000_pytorch_model.pt
 log_path=/mnt/workspace/junjin/code/starVLA/outputs/robotwin/0127_robotwin_Qwen3vlGR00T_vggt_cross_bs16_step10000
-
-mkdir -p ${LOG_DIR}
-seed=0
 task_config="demo_clean" # demo_randomized
+
+seed=0
 LOG_DIR="${log_path}/logs/${task_config}"
+mkdir -p ${LOG_DIR}
 ROBOTWIN_PATH=/mnt/workspace/junjin/code/RoboTwin
 policy_name="model2robotwin_interface"
 
@@ -33,8 +33,6 @@ export PYTHONPATH=$EVAL_FILES_PATH:$PYTHONPATH
 cd $ROBOTWIN_PATH
 
 echo "PYTHONPATH: $PYTHONPATH"
-
-
 
 # ckpt_setting ${ckpt_setting} \
 for i in "${!task_names[@]}"; do
