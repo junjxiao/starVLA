99d1e3802ecb98abd16fb1a9f26a2db67a9bbc42
时习 <xiaojunjin.xjj@alibaba-inc.com>
2026-01-31 11:31:12 +0800

    .

--- examples/Robotwin/eval_files/run_policy_server.sh
+++ examples/Robotwin/eval_files/run_policy_server.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools fro
 export star_vla_python=/mnt/workspace/junjin/conda/starvla/bin/python
 your_ckpt=/mnt/workspace/junjin/code/starVLA/checkpoints/0127_robotwin_Qwen3vlGR00T_vggt_cross_bs16/checkpoints/steps_10000_pytorch_model.pt
 gpu_id=3
-port=5695
+port=5696
 ################# star Policy Server ######################
 
 # export DEBUG=true
