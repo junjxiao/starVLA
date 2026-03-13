fbc8168cf23668787875306f04830b8d18acc1cd
时习 <xiaojunjin.xjj@alibaba-inc.com>
2026-03-13 18:10:38 +0800

    .

---
+++ examples/LIBERO/train_files/run_libero_notebook.sh
@@ -0,0 +1,85 @@
+eval "$(conda shell.bash hook)"
+# source activate
+conda activate python3.10
+
+run_id=0313_liberoall_Qwen3vlGR00T_vggt_longcat_view2_mmdit_cross_bs8_8gpus
+
+# === Please modify the following paths according to your environment ===
+Framework_name=QwenGR00TSpatial #QwenGR00T, QwenGR00TSpatial
+batch_size=8
+# freeze_module_list="qwen_vl_interface.model,spatial_model,qwen_image_edit_model.text_encoder,qwen_image_edit_model.transformer,qwen_image_edit_model.vae"
+freeze_module_list="spatial_model,image_edit_model"
+base_vlm=/mnt/workspace/zengshuang.zs/checkpoints/Qwen3-VL-4B-Instruct-Action
+config_yaml=./examples/LIBERO/train_files/starvla_cotrain_libero.yaml
+libero_data_root=/mnt/nas-data-3/yangyandan/libero
+data_mix=libero_all # libero_all
+run_root_dir=/mnt/workspace/junjin/code/starVLA/checkpoints
+# === End of environment variable configuration ===
+###########################################################################################
+
+
+
+# export CUDA_HOME=/usr/local/cuda-12
+# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+# export PATH=$CUDA_HOME/bin:$PATH
+export WANDB_MODE=offline
+export HF_HOME=/mnt/workspace/yangyandan/cache/huggingface 
+export HF_ENDPOINT=https://hf-mirror.com 
+export CHECKPOINT_BASEDIR=/mnt/workspace/zengshuang.zs/checkpoints
+
+output_dir=${run_root_dir}/${run_id}
+mkdir -p ${output_dir}
+# mv this script to the output dir
+# cp $0 ${output_dir}/
+
+
+# CUDA_VISIBLE_DEVICES=3 accelerate launch \
+#   --config_file starVLA/config/deepseeds/deepspeed_zero2.yaml \
+#   --num_processes 1 \
+#   starVLA/training/train_starvla.py \
+#   --config_yaml ${config_yaml} \
+#   --framework.name ${Framework_name} \
+#   --framework.qwenvl.base_vlm ${base_vlm} \
+#   --datasets.vla_data.data_root_dir ${libero_data_root}\
+#   --datasets.vla_data.data_mix ${data_mix} \
+#   --datasets.vla_data.per_device_batch_size 2 \
+#   --trainer.vla_data.video_backend torchvision_av \
+#   --trainer.freeze_modules ${freeze_module_list} \
+#   --trainer.max_train_steps 100000 \
+#   --trainer.save_interval 10000 \
+#   --trainer.logging_frequency 100 \
+#   --trainer.eval_interval 1000 \
+#   --run_root_dir ${run_root_dir} \
+#   --run_id ${run_id} \
+#   # --is_debug True
+
+
+
+accelerate launch starVLA/training/train_starvla.py \
+  --config_yaml ${config_yaml} \
+  --framework.name ${Framework_name} \
+  --framework.qwenvl.base_vlm ${base_vlm} \
+  --datasets.vla_data.data_root_dir ${libero_data_root}\
+  --datasets.vla_data.data_mix ${data_mix} \
+  --datasets.vla_data.per_device_batch_size ${batch_size} \
+  --datasets.vla_data.num_workers 4 \
+  --trainer.vla_data.video_backend torchvision_av \
+  --trainer.freeze_modules ${freeze_module_list} \
+  --trainer.max_train_steps 30000 \
+  --trainer.save_interval 5000 \
+  --trainer.logging_frequency 100 \
+  --trainer.eval_interval 1000 \
+  --run_root_dir ${run_root_dir} \
+  --run_id ${run_id} \
+  --wandb_entity junjin \
+  --wandb_project ${run_id}\
+  --framework.fuser.type cross_attention \
+  --framework.image_edit_model.view_num 2
+  # --trainer.pretrained_checkpoint /mnt/workspace/zengshuang.zs/output/pretrain/1223_oxe_pretrain_Qwen3VL4BFast/checkpoints/steps_48000_pytorch_model.pt \
+  # --trainer.reload_modules qwen_vl_interface \
+  # --framework.image_edit_model null
+  # --trainer.pretrained_checkpoint /mnt/workspace/junjin/code/starVLA/checkpoints/0109_liberoall_Qwen3vlGR00T_no_vggt_longcat_image_edit_cross_bs4_test2/checkpoints/steps_1000_pytorch_model.pt
+    # --framework.spatial_model null \
+  # --is_debug True
+
+
--- train_libero_all.sh
+++ train_libero_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# DEEPSPEED_CONFIG_FILE=starVLA/config/deepseeds/zero0.json,
+
 ENVS="CHECKPOINT_BASEDIR=/mnt/workspace/zengshuang.zs/checkpoints,WANDB_MODE=offline,HF_HOME=/mnt/workspace/yangyandan/cache/huggingface,HF_ENDPOINT=https://hf-mirror.com"
 
-run_id=03011_liberoall_Qwen3vlGR00T_vggt_longcat_view2_mmdit_cross_bs16_4gpus
+run_id=0311_liberoall_Qwen3vlGR00T_vggt_longcat_view2_mmdit_cross_bs16_4gpus
 args="--config_yaml ./examples/LIBERO/train_files/starvla_cotrain_libero.yaml \
       --framework.name QwenGR00TSpatial \
       --framework.qwenvl.base_vlm /mnt/workspace/zengshuang.zs/checkpoints/Qwen3-VL-4B-Instruct-Action \
@@ -22,36 +22,39 @@ args="--config_yaml ./examples/LIBERO/train_files/starvla_cotrain_libero.yaml \
       --framework.fuser.type cross_attention \
       --framework.image_edit_model.view_num 2 \ 
       "
-            # --datasets.vla_data.mv_data_root_dir /mnt/xlab-nas-1/junjin/dataset/libero_mv_images \
+      # --datasets.vla_data.mv_data_root_dir /mnt/xlab-nas-1/junjin/dataset/libero_mv_images \
       # --framework.spatial_model null \
-# --trainer.pretrained_checkpoint /mnt/workspace/zengshuang.zs/output/pretrain/1223_oxe_pretrain_Qwen3VL4BFast/checkpoints/steps_48000_pytorch_model.pt \
-# --trainer.reload_modules qwen_vl_interface
-# --trainer.pretrained_checkpoint /mnt/workspace/junjin/code/starVLA/checkpoints/0116_liberoall_Qwen3vlGR00T_vggt_longcat_image_edit_cross_bs16/checkpoints/steps_10000_pytorch_model.pt \
-
+      # --trainer.pretrained_checkpoint /mnt/workspace/zengshuang.zs/output/pretrain/1223_oxe_pretrain_Qwen3VL4BFast/checkpoints/steps_48000_pytorch_model.pt \
+      # --trainer.reload_modules qwen_vl_interface
+      # --trainer.pretrained_checkpoint /mnt/workspace/junjin/code/starVLA/checkpoints/0116_liberoall_Qwen3vlGR00T_vggt_longcat_image_edit_cross_bs16/checkpoints/steps_10000_pytorch_model.pt \
       # --trainer.resume_from_checkpoint null \
 
-# 打印将要传递的参数，方便调试
-echo "即将传递给训练脚本的参数："
-echo "${args}"
-echo ""
-echo "即将设置的环境变量："
-echo "${ENVS}"
-echo ""
 
 #amap_app_common_h20_nm125
 # amap-poi_ppu810e
 # amap_app_common_h20_na175
 # amap_app_vtspoi_h20
-nebulactl run mdl --queue=amap_app_vtspoi_h20 \
-                  --entry="starVLA/training/train_starvla.py" \
+# nebulactl run mdl --queue=amap_app_vtspoi_h20 \
+#                   --entry="starVLA/training/train_starvla.py" \
+#                   --algo_name=pytorch260 \
+#                   --worker_count=4 \
+#                   --user_params="$args" \
+#                   --file.cluster_file=./cluster.json \
+#                   --job_name="${run_id}" \
+#                   --nas_file_system_id=92bcb4b594-nvt70.cn-zhangjiakou.nas.aliyuncs.com,29016449f1c-mkq60.cn-wulanchabu.nas.aliyuncs.com,9dc4e499f2-tek11.cn-zhangjiakou.nas.aliyuncs.com,29e2cf482cb-cxw73.cn-wulanchabu.nas.aliyuncs.com \
+#                   --nas_file_system_mount_path=/mnt/workspace,/mnt/nas-data-3,/mnt/nas-data-1,/mnt/xlab-nas-1 \
+#                   --env="${ENVS}"
+
+nebulactl run mdl --queue=amap-poi_ppu810e \
+                  --entry="bash examples/LIBERO/train_files/run_libero_notebook.sh" \
                   --algo_name=pytorch260 \
-                  --worker_count=4 \
-                  --user_params="$args" \
-                  --file.cluster_file=./cluster.json \
-                  --job_name="${run_id}" \
+                  --worker_count=1 \
+                  --user_params="" \
+                  --file.cluster_file=./cluster_docker.json \
+                  --job_name="starVLA" \
                   --nas_file_system_id=92bcb4b594-nvt70.cn-zhangjiakou.nas.aliyuncs.com,29016449f1c-mkq60.cn-wulanchabu.nas.aliyuncs.com,9dc4e499f2-tek11.cn-zhangjiakou.nas.aliyuncs.com,29e2cf482cb-cxw73.cn-wulanchabu.nas.aliyuncs.com \
                   --nas_file_system_mount_path=/mnt/workspace,/mnt/nas-data-3,/mnt/nas-data-1,/mnt/xlab-nas-1 \
-                  --env="${ENVS}"
+                  --custom_docker_image=hub.docker.alibaba-inc.com/mdl/notebook_saved:xiaojunjin.xjj_libero_plus_ppu2_20260119180727
 
 # train depth head
 # base_vlm='/mnt/workspace/zengshuang.zs/checkpoints/Qwen3-VL-4B-Instruct'
